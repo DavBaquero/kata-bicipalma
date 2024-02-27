a7e070bdb359ba715fa2341bb14868523fe62874
DavBaquero <david.baquero.19@gmail.com>
2024-02-27 13:41:27 +0100

    🛡️ test : Add new file test(AnclajesTest)

---
+++ app/src/test/java/edu/badpals/bicipalma/domain/AnclajesTest.java
@@ -0,0 +1,15 @@
+package edu.badpals.bicipalma.domain;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class AnclajesTest {
+    
+     @Test
+    public void toStringTest() {
+        Anclajes anclajes = new Anclajes(6);
+        assertEquals("Numero de anclajes: 6", anclajes.toString());
+    } 
+    
+}
